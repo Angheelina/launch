1a4bc6640fd4fbbbc3ec1439c2728fe67385c306
Angie Blyden <adbowler88@gmail.com>
2020-12-29 12:23:11 -0600

    validation added

--- script.js
+++ script.js
@@ -11,3 +11,56 @@
 </ol>
 <img src="${}">
 */
+
+
+
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        if (isFuelLevelValid() && isCargoMassValid() && areFieldsFilled()) {
+            let faultyItems = document.querySelector("#faultyItems");
+            faultyItems.style.visibility = "visible";
+        }
+
+        event.preventDefault();
+    });
+});
+
+function isFuelLevelValid(){
+    let fuelLevelEl = document.querySelector("input[name=fuelLevel]");
+    let valid = isNumeric(fuelLevelEl.value)
+    if (!valid) {
+        alert("Fuel Level is invalid.");
+    }
+
+    return valid;
+}
+
+function isCargoMassValid() {
+    let cargoMassEl = document.querySelector("input[name=cargoMass]");
+    let valid = isNumeric(cargoMassEl.value)
+    if (!valid) {
+        alert("Cargo Mass is invalid.");
+    }
+
+    return valid;
+}
+
+function areFieldsFilled() {
+    let pilotNameEl = document.querySelector("input[name=pilotName]");
+    let copilotNameEl = document.querySelector("input[name=copilotName]");
+    let fuelLevelEl = document.querySelector("input[name=fuelLevel]");
+    let cargoMassEl = document.querySelector("input[name=cargoMass]");
+
+    if (pilotNameEl.value === "" || copilotNameEl.value === "" ||
+        fuelLevelEl.value === "" || cargoMassEl.value === "") {
+        alert("All fields must be filled in.");
+        return false;
+    }
+
+    return true;
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
